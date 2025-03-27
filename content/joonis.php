921af625762ac235d7a6617f78d8724430a878eb
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-03-27 13:18:00 +0200

    save

--- content/joonis.php
+++ content/joonis.php
@@ -3,12 +3,14 @@
 <br>
 <canvas id="housesmall" width="500" height="500"></canvas>
 <br>
-<input type="button" value="MAJAKE" onclick="drawnfullhouse()">
-<input type="button" value="rohi" onclick="grass()">
-<input type="button" value="sky" onclick="skyfun()">
-<input type="button" value="taevas" onclick="wall()">
-<input type="button" value="katus" onclick="roof()">
-<input type="button" value="aken" onclick="windowww()">
-<input type="button" value="korsten" onclick="chimney()">
-<input type="button" value="uks" onclick="doorfuc()">
-<input type="button" value="kustuta" onclick="removeMajake()">
+<div class="joonis">
+    <input type="button" value="MAJAKE" onclick="drawnfullhouse()">
+    <input type="button" value="rohi" onclick="grass()">
+    <input type="button" value="sky" onclick="skyfun()">
+    <input type="button" value="taevas" onclick="wall()">
+    <input type="button" value="katus" onclick="roof()">
+    <input type="button" value="aken" onclick="windowww()">
+    <input type="button" value="korsten" onclick="chimney()">
+    <input type="button" value="uks" onclick="doorfuc()">
+    <input type="button" value="kustuta" onclick="removeMajake()">
+</div>
