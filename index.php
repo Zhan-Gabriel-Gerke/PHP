ad43c781d179fdf52ecdefbbf7e753d67273740e
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-03-27 09:07:53 +0200

    1first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP index leht</title>
+</head>
+<body>
+<?php
+//php osa
+echo "Tere paevast!";
+?>
+</body>
+</html>
